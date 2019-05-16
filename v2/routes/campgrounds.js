478f5cb4155014bbe10d83a1d3c260b0f45d9a99
yunus <ynsture@gmail.com>
2019-05-16 20:34:18 +0000

    campgrounds refactored and user name added

--- v2/routes/campgrounds.js
+++ v2/routes/campgrounds.js
@@ -30,7 +30,8 @@ router.post("/", isLoggedIn,function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author={id:req.user._id,username:req.user.username};
+    var newCampground = {name: name, image: image, description: desc,author:author};
       /*=======================================
      Create a new campground and save to DB
      =========================================*/
