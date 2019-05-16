6e3ae971f12feaefe6306ade073882cb3249ddbd
yunus <ynsture@gmail.com>
2019-05-16 20:01:46 +0000

    comment section refactored

--- v2/routes/comments.js
+++ v2/routes/comments.js
@@ -23,10 +23,14 @@ router.post("/",isLoggedIn,(req,res)=>{
             console.log(err);
             res.redirect("/campgrounds")
         }else{
+            
             Comment.create(req.body.comment,(err,comment)=>{
                 if(err){
                     console.log(err);
                 }else{
+                    comment.author.id=req.user._id;
+                    comment.author.username=req.user.username;
+                    comment.save();
                     campground.comments.push(comment);
                     campground.save();
                     res.redirect("/campgrounds/" + campground._id)
